8adbe9823320701541b5bb63d14c1cfe10091623
unknown <prajwal.thite@uni-konstanz.de>
2025-02-10 19:48:29 +0100

    final_update_v6.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MapWithGeofencing from './MapWithGeofencing';
 import Page1 from './Page1';
 import Page2 from './Page2';
